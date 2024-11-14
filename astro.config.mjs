d83d53c79f6eb8433956ac7ca5a0a07e58244e2f
Lucas Bacelar De Souza <lucas.bacelar@grupoboticario.com.br>
2024-11-14 17:39:38 -0300

    fix: add correct astro output format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,18 @@
-import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
-import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
-import remarkUnwrapImages from 'remark-unwrap-images'
-import rehypeExternalLinks from 'rehype-external-links'
+import tailwind from '@astrojs/tailwind'
 import expressiveCode from 'astro-expressive-code'
-import { expressiveCodeOptions } from './src/site.config'
 import icon from 'astro-icon'
+import { defineConfig } from 'astro/config'
+import rehypeExternalLinks from 'rehype-external-links'
+import remarkUnwrapImages from 'remark-unwrap-images'
+import { expressiveCodeOptions } from './src/site.config'
+import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
 
-import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.me',
+	site: 'https://kiwidev808.github.io',
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
 		tailwind({
@@ -41,8 +40,5 @@ export default defineConfig({
 		}
 	},
 	prefetch: true,
-	output: 'server',
-	adapter: vercel({
-		webAnalytics: { enabled: true }
-	})
+	output: 'static',
 })
